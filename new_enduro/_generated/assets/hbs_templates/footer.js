3fb78f3f89bf57ee60daed6ad9b461fec48bab85
kristoferek <krzysztof.michal.nowak@gmail.com>
2018-12-09 13:30:51 +0100

    enduro install and uer admin set

---
+++ new_enduro/_generated/assets/hbs_templates/footer.js
@@ -0,0 +1,3 @@
+define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<section class=\"screen half\">\n	<div class=\"center_block\">\n		<p>For support visit enduro.js on <a href=\"https://github.com/Gottwik/Enduro\">github</a></p>\n	</div>\n</section>";
+},"useData":true}); });
---
+++ new_enduro/cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '187b88b7a8527142242b52f06664e6eb',
+			hash: '68b08449a6c5d452d7bdd12dc834d8394625cb26b53e791da6cbdbc50cc92ad3',
+			user_created_timestamp: 1544358458112
+		}
+	]
+})
